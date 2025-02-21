d192dbf1a9cecb353a42a331e01fd703279a45e3
Eryk Tatol <eryk.tatol@stud.techin.lt>
2025-02-21 08:57:07 +0200

    cleanup

--- server/src/main/java/lt/techin/server/trip_application/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.techin.server.trip_application.repository;
-
-import lt.techin.server.trip_application.model.UserTrip;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<UserTrip, Long> {
-
-
-}
